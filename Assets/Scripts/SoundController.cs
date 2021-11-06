d989d7c184069ab32170c8349f679092ca6af237
Umbracon <lastepitaphium@gmail.com>
2021-11-06 20:36:17 +0100

    added option toggles' functionality

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -1,15 +1,16 @@
-
-
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundController : MonoBehaviour
 {
-
     [SerializeField] AudioSource musicAudioSource;
     [SerializeField] AudioSource soundEffectsAudioSource;
 
     [SerializeField] AudioClip snakeBiteClip;
     [SerializeField] AudioClip buttonClip;
+
+    [SerializeField] AudioMixer musicAudioMixer;
+    [SerializeField] AudioMixer soundEffectsAudioMixer;
     
     static SoundController controller = null;
     public static SoundController Controller 
@@ -31,6 +32,37 @@ public class SoundController : MonoBehaviour
         PlayBackgroundMusic();
     }
 
+    public void ToggleMusicMixer()
+    {
+        float value;
+        musicAudioMixer.GetFloat("MusicMixerVolume", out value);
+
+        if (value == 0f)
+        {
+            musicAudioMixer.SetFloat("MusicMixerVolume", -80f);
+        }
+        else
+        {
+            musicAudioMixer.SetFloat("MusicMixerVolume", 0f);
+        }
+    }
+    
+    public void ToggleSoundEffectsMixer()
+    {
+        float value;
+        soundEffectsAudioMixer.GetFloat("SoundEffectsMixerVolume", out value);
+
+        if (value == 0f)
+        {
+            soundEffectsAudioMixer.SetFloat("SoundEffectsMixerVolume", -80f);
+        }
+        else
+        {
+            soundEffectsAudioMixer.SetFloat("SoundEffectsMixerVolume", 0f);
+            soundEffectsAudioSource.PlayOneShot(buttonClip);
+        }
+    }
+
     public void PlayButtonSound()
     {
         soundEffectsAudioSource.PlayOneShot(buttonClip);
@@ -41,7 +73,7 @@ public class SoundController : MonoBehaviour
         soundEffectsAudioSource.PlayOneShot(snakeBiteClip);
     }
 
-    public void PlayBackgroundMusic()
+    void PlayBackgroundMusic()
     {
         musicAudioSource.Play();
     }
