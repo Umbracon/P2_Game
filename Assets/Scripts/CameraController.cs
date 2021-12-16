6ff85e263937afaa8be1261616bf3f61c2292222
Umbracon <umbracon@empyrean.games>
2021-12-16 16:54:58 +0100

    tinkered with camera rotation clamping

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 using Cinemachine;
 
 public class CameraController : MonoBehaviour {
-    [SerializeField] float yRotationSpeed = 0.6f;
-    [SerializeField] float zRotationSpeed = 0.5f;
+    [SerializeField] float yRotationSpeed = 60f;
+    [SerializeField] float zRotationSpeed = 50f;
     [SerializeField] float zRotationLimit = 70f;
     CinemachineVirtualCamera virtualCamera;
-    float horizontal;
-    float vertical;
+    float yRotation;
+    float zRotation;
     float scroll;
 
     void Start() {
@@ -16,14 +16,31 @@ public class CameraController : MonoBehaviour {
     }
 
     void Update() {
+        yRotation += -Input.GetAxis("Horizontal") * yRotationSpeed * Time.deltaTime ;
+        zRotation += Input.GetAxis("Vertical") * zRotationSpeed * Time.deltaTime ;
+        scroll = Input.mouseScrollDelta.y;
+        
+        transform.rotation = Quaternion.Euler(0, yRotation, Mathf.Clamp(zRotation, zRotationLimit * -1, zRotationLimit) );
+        
+        Debug.Log(Time.deltaTime);
+    }
+    /*void Update() {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         scroll = Input.mouseScrollDelta.y;
         if (Mathf.Abs(horizontal) > 0.1f)
             transform.Rotate(new Vector3(0, -horizontal * yRotationSpeed, 0), Space.World);
-        if (Mathf.Abs(vertical) > 0.1f && Math.Abs(transform.rotation.y) < zRotationLimit)
+        if (Mathf.Abs(vertical) > 0.1f) {
             transform.Rotate(new Vector3(0, 0, vertical * zRotationSpeed), Space.Self);
+            transform.eulerAngles.z 
+        }
+
         if (Mathf.Abs(scroll) > 0.1f)
             virtualCamera.m_Lens.FieldOfView -= scroll;
+    }
+    */
+
+    static float GetNegativeRotationValues(float angle) {
+        return (angle > 180) ? angle - 360 : angle;
     }
 }
