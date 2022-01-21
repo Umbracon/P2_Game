1b0f597dfc9fa88e301b2a4b8bae9bf512e4b0d2
Umbracon <umbracon@empyrean.games>
2022-01-21 17:13:22 +0100

    fixed camera controller script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,39 +6,46 @@ public class CameraController : MonoBehaviour {
     [SerializeField] float yRotationSpeed = 60f;
     [SerializeField] float zRotationSpeed = 50f;
     [SerializeField] float zRotationLimit = 70f;
+    [SerializeField] float zoomInLimit = 25f;
+    [SerializeField] float zoomOutLimit = 110f;
+    [SerializeField] float cameraDamping = 5f;
     CinemachineVirtualCamera virtualCamera;
     float yRotation;
     float zRotation;
     float scroll;
-
+    float cameraDistance;
+    
     void Start() {
         virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
+        cameraDistance = virtualCamera.m_Lens.FieldOfView;
     }
 
     void Update() {
-        yRotation += -Input.GetAxis("Horizontal") * yRotationSpeed * Time.deltaTime ;
-        zRotation += Input.GetAxis("Vertical") * zRotationSpeed * Time.deltaTime ;
-        scroll = Input.mouseScrollDelta.y;
-        
-        transform.rotation = Quaternion.Euler(0, yRotation, Mathf.Clamp(zRotation, zRotationLimit * -1, zRotationLimit) );
+        CalculateYRotation();
+        CalculateZRotation();
+        RotateCamera();
+        CalculateZoomDistance();
+        Zoom();
     }
-    /*void Update() {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-        scroll = Input.mouseScrollDelta.y;
-        if (Mathf.Abs(horizontal) > 0.1f)
-            transform.Rotate(new Vector3(0, -horizontal * yRotationSpeed, 0), Space.World);
-        if (Mathf.Abs(vertical) > 0.1f) {
-            transform.Rotate(new Vector3(0, 0, vertical * zRotationSpeed), Space.Self);
-            transform.eulerAngles.z 
-        }
 
-        if (Mathf.Abs(scroll) > 0.1f)
-            virtualCamera.m_Lens.FieldOfView -= scroll;
+    void CalculateYRotation() {
+        yRotation += -Input.GetAxis("Horizontal") * yRotationSpeed * Time.deltaTime;
     }
-    */
 
-    static float GetNegativeRotationValues(float angle) {
-        return (angle > 180) ? angle - 360 : angle;
+    void CalculateZRotation() {
+        zRotation += Input.GetAxis("Vertical") * zRotationSpeed * Time.deltaTime;
+        zRotation = Mathf.Clamp(zRotation, zRotationLimit * -1, zRotationLimit);
+    }
+
+    void RotateCamera() {
+        transform.rotation = Quaternion.Euler(0, yRotation, zRotation);
+    }
+
+    void CalculateZoomDistance() {
+        cameraDistance -= Input.mouseScrollDelta.y;
+        cameraDistance = Mathf.Clamp(cameraDistance, zoomInLimit, zoomOutLimit);
+    }
+    void Zoom() {
+        virtualCamera.m_Lens.FieldOfView = Mathf.Lerp(virtualCamera.m_Lens.FieldOfView, cameraDistance, cameraDamping * Time.deltaTime);
     }
 }
